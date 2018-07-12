dbb32480a71ef674b26eacc6828bcae04adf3332
Vishnu V P (vivp) <vivp@cisco.com>
2018-07-13 01:03:07 +0530

    Improvements

--- ping.py
+++ ping.py
@@ -11,19 +11,23 @@ if localhost in ipList:
 	ipList.remove(localhost)
 
 while True:
+
 	return_text = ""
-	for ip in ipList:
-	    res = subprocess.Popen(['ping', '-c', '3', ip],stdout=subprocess.PIPE)
-	    stdout, stderr = res.communicate()
-	    if res.returncode == 0:
-	            #return_text += '<div>' + ipDict[ip].split(".")[0].upper() + " .....<span style='color:green'> Connected</span></div><br />"
-	            return_text += ipDict[ip].split(".")[0].upper() + " ...... Connected\n"
-	    elif res.returncode == 2:
-	            return_text += ipDict[ip].split(".")[0].upper() + " ...... Unreachable\n"
-	    else:
-	            return_text += ipDict[ip].split(".")[0].upper() + " ...... Unreachable\n"
-	#return_text += "</body></html>"
-	print(return_text)
+        for ip in ipList:
+            res = subprocess.Popen(['ping', '-w', '1', '-c', '1', ip],stdout=subprocess.PIPE)
+            stdout, stderr = res.communicate()
+            if res.returncode == 0:
+                    #return_text += '<div>' + ipDict[ip].split(".")[0].upper() + " .....<span style='color:green'> Connected</span></div><br />"
+                    return_text += ipDict[ip].split(".")[0].upper() + " ...... Connected\n"
+            elif res.returncode == 2:
+                    return_text += ipDict[ip].split(".")[0].upper() + " ...... Unreachable\n"
+            else:
+                    return_text += ipDict[ip].split(".")[0].upper() + " ...... Unreachable\n"
+        #return_text += "</body></html>"
+        # res.kill()
+        print(return_text)
+        time.sleep(1)
+
 
 
 
